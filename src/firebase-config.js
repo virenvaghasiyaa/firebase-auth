a23a95e040295e4602ef3d1e74b31693148bf26f
jarvis <code.viren@gmail.com>
2022-09-05 16:32:53 +0530

    env removed

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDj789MlXvHjTce7dHUUbzR1KvNN--jrhc",
+  authDomain: "fir-auth-46fc3.firebaseapp.com",
+  projectId: "fir-auth-46fc3",
+  storageBucket: "fir-auth-46fc3.appspot.com",
+  messagingSenderId: "337232192007",
+  appId: "1:337232192007:web:1fae41cd93b82a246fc0a2"
 };
 
   
